199bf4462b163b32ad733c864f1a7a7f870633df
Kevin Desousa <kevin.desousa1@uoit.net>
2019-03-01 20:31:05 -0500

    another change made to the test script, and changed the transaction output

--- test-runner.sh
+++ test-runner.sh
@@ -3,7 +3,12 @@
 
 
 tests="tests/*"
+
+rm test_log.txt
 touch test_log.txt
+
+rm test_verif.txt
+touch test_verif.txt
 
 for t in $tests
 do
@@ -11,12 +16,25 @@ do
     # If its a directory, go inside of it
     if [ -d "$t" ]
     then 
+
         # Run the script and dump the output in the folder to be compared
-        ./ticket-seller users.ua stock.at $t/trans.out < $t/test.inp
+        ./ticket-seller tests/users.ua tests/stock.at $t/trans.out < $t/test.inp
         
         echo "TEST $t:" >> test_log.txt
         diff $t/trans.out $t/test.out >> test_log.txt
         echo $'\n' >> test_log.txt
+
+        # Write a file documenting if the file is ok or not
+        if [ "diff $t/trans.out $t/test.out" == "" ]
+        then
+            echo "TEST $t: GOOD" >> test_verif.txt
+        else
+            echo "TEST $t: BAD" >> test_verif.txt
+        fi
+
+
+
+        rm $t/trans.out
     fi
  
 done
